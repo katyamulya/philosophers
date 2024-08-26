30f6f856704b42f2e41b4f1f84310caf9292497c
Katsiaryna Dvarakouskaya <kdvarako@2-F-6.42heilbronn.de>
2024-08-26 17:34:41 +0200

    added check_err.c

---
+++ philo/utils/check_err.c
@@ -0,0 +1,98 @@
+#include "../philo.h"
+//#include <limits.h>
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	ft_atoi(const char *str)
+{
+	int		i;
+	long	num;
+
+	i = 0;
+	num = 0;
+	while (str[i] >= '0' && str[i] <= '9' && str[i] != '\0')
+	{
+		num = num * 10;
+		num = num + str[i] - 48;
+		i++;
+	}
+	//printf("%lu\n", num);
+	if (num < -2147483648 || num > 2147483647)
+		num = 0;
+	return (num);
+}
+
+int	check_ifnum(const char *s)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if ((s[i] < '0' || s[i] > '9'))
+			return (-1);
+		i++;
+	}
+	return (0);
+}
+
+int	check_edge_cases(int argc, char **argv)
+{
+	if (ft_atoi(argv[1]) < 1 || ft_atoi(argv[1]) > 200)
+		return (-1);
+	if (ft_atoi(argv[2]) < 60)
+		return (-1);
+	if (ft_atoi(argv[3]) < 60)
+		return (-1);
+	if (ft_atoi(argv[4]) < 60)
+		return (-1);
+	if (argc == 6)
+	{
+		if (ft_atoi(argv[5]) < 0)
+			return (-1);
+	}
+	return (0);
+}
+
+int check_err(int argc, char **argv)
+{
+	// all should be numbers > 0, number_eat can be 0, number_philos <= 200
+	int i;
+
+	i = 1;
+	while (argv[i] != NULL)
+	{
+		if (check_ifnum(argv[i]) == -1)
+		{
+			printf("Error! Args should be positive numbers\n");
+			return (-1);
+		}
+		i++;
+	}
+	i = 1;
+	while (argv[i] != NULL)
+	{
+		//printf("%d\n", ft_atoi(argv[i]));
+	   // printf("len: %zu\n", ft_strlen(argv[i]));
+		if (ft_atoi(argv[i]) == 0 && ft_strlen(argv[i]) > 1)
+		{
+			printf("Error! Wrong data\n");
+			return (-1);
+		}
+		i++;
+	}
+	if (check_edge_cases(argc, argv) == -1)
+	{
+			printf("Error! Edge cases\n");
+			return (-1);
+	}
+	return (0);
+}
