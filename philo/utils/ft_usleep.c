7435ef072feb90d11bd0737973b9dfe4fd502cad
Katsiaryna Dvarakouskaya <kdvarako@1-C-1.42heilbronn.de>
2024-09-05 17:07:54 +0200

    modif ft_usleep.c

--- philo/utils/ft_usleep.c
+++ philo/utils/ft_usleep.c
@@ -6,7 +6,7 @@
 /*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 15:21:12 by kdvarako          #+#    #+#             */
-/*   Updated: 2024/08/26 15:23:28 by kdvarako         ###   ########.fr       */
+/*   Updated: 2024/09/05 16:51:34 by kdvarako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,19 @@ void	ft_usleep(int ms)
 
 	gettimeofday(&start, 0);
 	gettimeofday(&after, 0);
-
-	while (((after.tv_sec - start.tv_sec) * 1000 + \
-		(after.tv_usec - start.tv_usec) / 1000) < ms)
+	//while (((after.tv_sec - start.tv_sec) * 1000 + \
+		//(after.tv_usec - start.tv_usec) / 1000) < ms)
+	while ((convert_ms(after) - convert_ms(start)) < ms)
 	{
-		usleep(10);
-		gettimeofday(&after, 0);
+		usleep(500);
+		gettimeofday(&after, NULL);
 	}
+}
+
+size_t	convert_ms(struct timeval time)
+{
+	size_t	time_ms;
+
+	time_ms = time.tv_sec * 1000 + time.tv_usec / 1000;
+	return (time_ms);
 }
