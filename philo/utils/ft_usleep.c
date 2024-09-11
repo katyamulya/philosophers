b4d0217e67aaf8155db0ac297bc5658c13518aec
Katsiaryna Dvarakouskaya <kdvarako@2-F-6.42heilbronn.de>
2024-09-11 11:21:32 +0200

    mofid ft_usleep.c, added head

--- philo/utils/ft_usleep.c
+++ philo/utils/ft_usleep.c
@@ -6,7 +6,7 @@
 /*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 15:21:12 by kdvarako          #+#    #+#             */
-/*   Updated: 2024/09/05 16:51:34 by kdvarako         ###   ########.fr       */
+/*   Updated: 2024/09/11 11:13:14 by kdvarako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@ void	ft_usleep(int ms)
 
 	gettimeofday(&start, 0);
 	gettimeofday(&after, 0);
-	//while (((after.tv_sec - start.tv_sec) * 1000 + \
-		//(after.tv_usec - start.tv_usec) / 1000) < ms)
 	while ((convert_ms(after) - convert_ms(start)) < ms)
 	{
 		usleep(500);
