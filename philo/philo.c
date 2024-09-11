e5ec18d776b040d484c4ba56a71c5181110110cd
Katsiaryna Dvarakouskaya <kdvarako@2-F-6.42heilbronn.de>
2024-09-11 11:16:30 +0200

    modified philo.c

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 13:38:25 by kdvarako          #+#    #+#             */
-/*   Updated: 2024/08/26 17:28:35 by kdvarako         ###   ########.fr       */
+/*   Updated: 2024/09/11 10:21:13 by kdvarako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,32 +32,17 @@ int	main(int argc, char **argv)
 	t_philo			philos[300];
 	pthread_mutex_t	forks[300];
 	t_prog_data		*data;
-	int				i;
 
 	data = NULL;
-	i = 0;
 	if (argc >= 5 && argc <= 6)
 	{
 		if (check_err(argc, argv) == 0)
 		{
 			data = init_data(argc, argv, philos);
-			//printf("aftervinit number_philos = %d\n", data->number_philos);
 			if (data != NULL)
 			{
-				/*printf("num = %d\n", data->number_philos);
-				printf("time_to_die = %zu\n", data->t_die);
-				printf("time_to_eat = %zu\n", data->t_eat);
-				printf("time_to_sleep = %zu\n", data->t_sleep);
-				printf("num_eat = %d\n", data->number_eat);
-				printf("count_eat = %d\n", data->count_eaten);*/
 				init_forks(forks, data->number_philos);
 				init_philos(philos, data, forks);
-				/*i = 0;
-				while (i < data->number_philos)
-				{
-					printf("philos[%d]: %d\n", i, philos[i].num);
-					i++;
-				}*/
 				/*if (create_threads(data, philos, data->number_philos) == 1)
 					return (1);*/
 				create_threads(*data, philos, data->number_philos);
