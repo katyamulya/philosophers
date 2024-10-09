3092befc4afa69a0cd5d50bac023565d660984bc
Katsiaryna Dvarakouskaya <kdvarako@2-F-6.42heilbronn.de>
2024-10-09 15:53:03 +0200

    norms

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 13:38:25 by kdvarako          #+#    #+#             */
-/*   Updated: 2024/09/11 10:21:13 by kdvarako         ###   ########.fr       */
+/*   Updated: 2024/10/09 15:26:05 by kdvarako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,25 +34,20 @@ int	main(int argc, char **argv)
 	t_prog_data		*data;
 
 	data = NULL;
-	if (argc >= 5 && argc <= 6)
+	if (argc >= 5 && argc <= 6 && check_err(argc, argv) == 0)
 	{
-		if (check_err(argc, argv) == 0)
+		data = init_data(argc, argv, philos);
+		if (data != NULL)
 		{
-			data = init_data(argc, argv, philos);
-			if (data != NULL)
-			{
-				init_forks(forks, data->number_philos);
-				init_philos(philos, data, forks);
-				/*if (create_threads(data, philos, data->number_philos) == 1)
-					return (1);*/
-				create_threads(*data, philos, data->number_philos);
-				destroy_mutexes(data, forks);
-			}
+			init_forks(forks, data->number_philos);
+			init_philos(philos, data, forks);
+			start_threads(*data, philos, data->number_philos);
+			destroy_mutexes(data, forks);
 		}
 	}
 	else
 	{
-		printf("Error! Wrong amount of args\n");
+		printf("Error! Incorrectly entered data.\n");
 		return (-1);
 	}
 	return (0);
